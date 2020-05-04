99d0bc605d9ca5e3355b334a217747155ccaf0ea
Shashi Shekhar <Shekhars094@gmail.com>
2020-05-04 18:14:49 +0530

    Submitiing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <h1>News Social App</h1>;
+	return <h1>News Social App !! It Will Be Live
+    Soon !!!!
+  </h1>;
 }
 
 export default App;
